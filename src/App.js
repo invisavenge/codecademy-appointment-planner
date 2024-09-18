b5d91cd3ec930305e6958518c92645723984a6e9
invisavenge <stuart91194@gmail.com>
2024-09-18 18:15:51 +0100

    setup appointments and contacts state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
-function App() {
+const App = () => {
   /*
-  Define state variables for 
-  contacts and appointments 
+    Define state variables for 
+    contacts and appointments 
   */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
   /*
-  Implement functions to add data to
-  contacts and appointments
+    Implement functions to add data to
+    contacts and appointments
   */
+  const addAppointment = appointment => {
+    const { name, contact, date, time } = appointment;
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
-    </Route>
-  ));
-  
+    if (!name || !contact || !date || !time) {
+      return;
+    }
+
+    setAppointments(prevAppointmentsArray => {
+      return [...prevAppointmentsArray, appointment];
+    });
+  };
+
+  const addContact = contact => {
+    const { name, phoneNumber, email } = contact;
+
+    if (!name || !phoneNumber || !email) {
+      return;
+    }
+
+    setContacts(prevContactsArray => {
+      return [...prevContactsArray, contact];
+    });
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={ <Root /> }>
+        <Route
+          index
+          element={ <Navigate to={ROUTES.CONTACTS} replace /> }
+        />
+
+        <Route
+          path={ROUTES.APPOINTMENTS}
+          element={ <AppointmentsPage appointments={appointments} addAppointment={addAppointment} /> }
+        />
+
+        <Route
+          path={ROUTES.CONTACTS}
+          element={ <ContactsPage contacts={contacts} addContact={addContact} /> }
+        />
+      </Route>
+    )
+  );
+
   return (
     <RouterProvider router={router}/>
   );
