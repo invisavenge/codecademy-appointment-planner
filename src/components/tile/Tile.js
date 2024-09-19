efdda6ad7d711244b2fecb18841b2c42da6190fc
invisavenge <stuart91194@gmail.com>
2024-09-19 22:34:02 +0100

    create TileList and Tile stateless components

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
+  // Object.values(description) returns an array containing the VALUES of each of the keys
+  // on the description object. I then map over the array to return a paragraph
+  // for each of the values.
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {description && Object.values(description).map((value, index) => {
+        return <p key={index}>{value}</p>;
+      })}
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-export const TileList = () => {
+import { Tile } from '../tile/Tile';
+
+export const TileList = ({tiles}) => {
   return (
     <div>
-      
+      {/* Only if tiles exists */}
+      {tiles && tiles.map((tile, index) => {
+        // Use object restructuring to extract the name property
+        // from each object and use ...rest syntax to get
+        // the description.
+        const { name, ...remainingTileProperties } = tile;
+
+        return (
+          <Tile
+            key={index}
+            name={name}
+            description={remainingTileProperties}
+          />
+        );
+      })}
     </div>
   );
 };
